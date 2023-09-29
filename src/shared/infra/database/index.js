b6f07a426c088636712c31225d714d2897ef0975
Andre Lima <andresdslima@gmail.com>
2023-09-29 21:46:10 +0200

    refactor: comment DB_DIALECT

--- src/shared/infra/database/index.js
+++ src/shared/infra/database/index.js
@@ -9,7 +9,7 @@ try {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      dialect: process.env.DB_DIALECT,
+      // dialect: process.env.DB_DIALECT,
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
     },
